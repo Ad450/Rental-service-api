0e51590d74c40188af78967f1631ad962a3b60a8
Ad450 <adjeimanue@gmail.com>
2022-07-11 19:43:36 +0000

    initial server setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hey, you must work ");
+import express from "express";
+import * as dotenv from "dotenv";
+import bodyParser from "body-parser";
+
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.listen(process.env.SERVER_PORT, () => {
+    console.log("sever started on port " + process.env.SERVER_PORT + ", more fire");
+})

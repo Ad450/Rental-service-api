2148d4077f95cb499e7be694d91cf5db88d7fa3c
Ad450 <adjeimanue@gmail.com>
2022-07-13 10:22:39 +0000

    database service interface added

---
+++ src/db/db_service.ts
@@ -0,0 +1,10 @@
+import { DbReturnType } from "../interfaces/database_service_param";
+
+export default abstract class DatabaseService<DatabaseParam> {
+    abstract create(param: DatabaseParam): Promise<void>;
+    abstract update(param: DatabaseParam): Promise<void>;
+    abstract delete(param: DatabaseParam): Promise<void>;
+    abstract get(param: DatabaseParam): Promise<DbReturnType | null>;
+    abstract getAll(param: DatabaseParam): Promise<Array<DbReturnType> | null>;
+}
+

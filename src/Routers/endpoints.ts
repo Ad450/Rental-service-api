14cdd03f18401d3ecb13d846eb15bc0a3bb69ca3
Ad450 <adjeimanue@gmail.com>
2022-07-14 15:01:43 +0000

    initial endpoint routes configured

--- src/Routers/endpoints.ts
+++ src/Routers/endpoints.ts
@@ -1,4 +1,6 @@
-import EndpointsInterface from "./endpoints_interface";
+import { NextFunction, Request, Response } from "express";
+import Injector from "../di/injector";
+import EndpointsInterface, { routerMethods } from "./endpoints_interface";
 
 // Rentals
 const getAllBooks: string = "/getAllBooks";
@@ -8,7 +10,6 @@ const turnInBook: string = "/turnInBook";
 
 // Auth
 const getAllUsers: string = "/getAllUsers";
-const getUsers: string = "/getUsers";
 const getUser: string = "/getUser:id";
 const signup: string = "/signup";
 const login: string = "/login";
@@ -16,7 +17,53 @@ const login: string = "/login";
 
 
 export const routers: Array<EndpointsInterface> = [
-    // {
-    //     route: 
-    // }
+    /// Authentication
+    {
+        route: signup,
+        method: routerMethods.POST,
+        handlers: (req: Request, res: Response, next: NextFunction) => Injector.signup.call({ req, res, next })
+    },
+    {
+        route: login,
+        method: routerMethods.POST,
+        handlers: (req: Request, res: Response, next: NextFunction) => Injector.login.call({ req, res, next })
+    },
+    {
+        route: getUser,
+        method: routerMethods.GET,
+        handlers: (req: Request, res: Response, next: NextFunction) => Injector.getUser.call({ req, res, next })
+    },
+    {
+        route: getAllUsers,
+        method: routerMethods.GET,
+        handlers: (req: Request, res: Response, next: NextFunction) => Injector.getAllUsers.call({ req, res, next })
+    },
+    {
+        route: getUser,
+        method: routerMethods.GET,
+        handlers: (req: Request, res: Response, next: NextFunction) => Injector.getUser.call({ req, res, next })
+    },
+
+    /// Rental Service
+    {
+        route: rentBook,
+        method: routerMethods.POST,
+        handlers: (req: Request, res: Response, next: NextFunction) => Injector.rentBook.call({ req, res, next })
+    },
+    {
+        route: turnInBook,
+        method: routerMethods.POST,
+        handlers: (req: Request, res: Response, next: NextFunction) => Injector.turnInBook.call({ req, res, next })
+    },
+    {
+        route: getBook,
+        method: routerMethods.GET,
+        handlers: (req: Request, res: Response, next: NextFunction) => Injector.getBook.call({ req, res, next })
+    },
+    {
+        route: getAllBooks,
+        method: routerMethods.GET,
+        handlers: (req: Request, res: Response, next: NextFunction) => Injector.getAllBooks.call({ req, res, next })
+    }
+
 ]

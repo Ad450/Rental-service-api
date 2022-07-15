0f388d46994b15e5d1d4f235925f5f98e170fa8d
Ad450 <adjeimanue@gmail.com>
2022-07-15 14:00:59 +0000

    added middlewares

--- src/Routers/endpoints.ts
+++ src/Routers/endpoints.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import Injector from "../di/injector";
+import { refineAuthInput, validateAuthInput, validateRentalInput, validateToken } from "../middlewares/validators";
 import EndpointsInterface, { routerMethods } from "./endpoints_interface";
 
 // Rentals
@@ -22,44 +23,52 @@ export const routers: Array<EndpointsInterface> = [
     {
         route: signup,
         method: routerMethods.POST,
-        handlers: (req: Request, res: Response, next: NextFunction) => Injector.signup.call({ req, res, next })
+        handlers: (req: Request, res: Response, next: NextFunction) => Injector.signup.call({ req, res, next }),
+        middlewares: [refineAuthInput, validateAuthInput]
     },
     {
         route: login,
         method: routerMethods.POST,
-        handlers: (req: Request, res: Response, next: NextFunction) => Injector.login.call({ req, res, next })
+        handlers: (req: Request, res: Response, next: NextFunction) => Injector.login.call({ req, res, next }),
+        middlewares: [refineAuthInput, validateToken, validateAuthInput]
     },
     {
         route: getUser,
         method: routerMethods.GET,
-        handlers: (req: Request, res: Response, next: NextFunction) => Injector.getUser.call({ req, res, next })
+        handlers: (req: Request, res: Response, next: NextFunction) => Injector.getUser.call({ req, res, next }),
+        middlewares: [validateToken, validateAuthInput]
     },
     {
         route: getAllUsers,
         method: routerMethods.GET,
-        handlers: (req: Request, res: Response, next: NextFunction) => Injector.getAllUsers.call({ req, res, next })
+        handlers: (req: Request, res: Response, next: NextFunction) => Injector.getAllUsers.call({ req, res, next }),
+        middlewares: [validateToken, validateAuthInput]
     },
 
     /// Rental Service
     {
         route: rentBook,
         method: routerMethods.POST,
-        handlers: (req: Request, res: Response, next: NextFunction) => Injector.rentBook.call({ req, res, next })
+        handlers: (req: Request, res: Response, next: NextFunction) => Injector.rentBook.call({ req, res, next }),
+        middlewares: [validateToken, validateRentalInput]
     },
     {
         route: turnInBook,
         method: routerMethods.POST,
-        handlers: (req: Request, res: Response, next: NextFunction) => Injector.turnInBook.call({ req, res, next })
+        handlers: (req: Request, res: Response, next: NextFunction) => Injector.turnInBook.call({ req, res, next }),
+        middlewares: [validateToken, validateRentalInput]
     },
     {
         route: getBook,
         method: routerMethods.GET,
-        handlers: (req: Request, res: Response, next: NextFunction) => Injector.getBook.call({ req, res, next })
+        handlers: (req: Request, res: Response, next: NextFunction) => Injector.getBook.call({ req, res, next }),
+        middlewares: [validateToken, validateRentalInput]
     },
     {
         route: getAllBooks,
         method: routerMethods.GET,
-        handlers: (req: Request, res: Response, next: NextFunction) => Injector.getAllBooks.call({ req, res, next })
+        handlers: (req: Request, res: Response, next: NextFunction) => Injector.getAllBooks.call({ req, res, next }),
+        middlewares: [validateToken, validateRentalInput]
     }
 
 ]
--- src/Routers/endpoints_interface.ts
+++ src/Routers/endpoints_interface.ts
@@ -4,8 +4,13 @@ export default interface EndpointsInterface {
     route: string,
     method: routerMethods,
     handlers: (req: Request, res: Response, next: NextFunction) => {}
+    middlewares: Array<(req: Request, res: Response, next: NextFunction) => {}>
 }
 
+
+// export interface IMiddleware {
+//     middleware: (req: Request, res: Response, next: NextFunction) => {}
+// }
 
 export enum routerMethods {
     POST,

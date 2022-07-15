e07cf2f7b4038fd9d8fb920fc4410cc35f827026
Ad450 <adjeimanue@gmail.com>
2022-07-15 16:05:39 +0000

    minor addition

--- src/di/injector.ts
+++ src/di/injector.ts
@@ -1,6 +1,8 @@
+import DatabaseService from "../db/db_service";
 import DatabaseServiceImpl from "../db/db_service_impl";
 import AuthServiceHandler from "../handlers/auth_service_handler";
 import RentalServiceHandler from "../handlers/rental_service_handler";
+import { DatabaseParam } from "../interfaces/database_service_param";
 import AuthServiceImpl from "../services/auth_service/auth_service_impl";
 import RentalServiceImpl from "../services/rental_service/rental_service_impl";
 import GetAllUsers from "../usecases/auth_usecases/get_all_users";
@@ -13,6 +15,8 @@ import RentBook from "../usecases/rental_usecases/rent_book";
 import TurnInBook from "../usecases/rental_usecases/turn_in_book";
 
 export default abstract class Injector {
+    /// usecases
+
     static login = new Login(new AuthServiceImpl(new AuthServiceHandler(new DatabaseServiceImpl())));
     static signup = new Signup(new AuthServiceImpl(new AuthServiceHandler(new DatabaseServiceImpl())));
     static getUser = new GetUser(new AuthServiceImpl(new AuthServiceHandler(new DatabaseServiceImpl())));
@@ -21,4 +25,7 @@ export default abstract class Injector {
     static getAllBooks = new GetAllBooks(new RentalServiceImpl(new RentalServiceHandler(new DatabaseServiceImpl())));
     static rentBook = new RentBook(new RentalServiceImpl(new RentalServiceHandler(new DatabaseServiceImpl())));
     static turnInBook = new TurnInBook(new RentalServiceImpl(new RentalServiceHandler(new DatabaseServiceImpl())));
+
+    /// database
+    static db = new DatabaseServiceImpl();
 }

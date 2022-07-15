88921eb438b2544a33aa46d20939770288440479
Ad450 <adjeimanue@gmail.com>
2022-07-15 16:05:59 +0000

    more refactoring

--- src/handlers/auth_service_handler.ts
+++ src/handlers/auth_service_handler.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import { comparePasswords, encryptData, generateAccessToken } from "../core/helpers";
+import { comparePasswords, encryptData, generateAccessToken, hashData } from "../core/helpers";
 import DatabaseService from "../db/db_service";
 import { DatabaseParam, UserParam } from "../interfaces/database_service_param";
 
@@ -12,7 +12,7 @@ export default class AuthServiceHandler {
 
     async signup(req: Request, res: Response, next: NextFunction): Promise<void> {
         // hash password with bycrypt and insert user data into db
-        const encryptedPassword = await encryptData(req.body.password);
+        const encryptedPassword = await hashData(req.body.password);
 
         const userData: UserParam = {
             email: req.body.email,
@@ -38,33 +38,16 @@ export default class AuthServiceHandler {
     }
 
     async login(req: Request, res: Response, next: NextFunction): Promise<void> {
-
-        // double check password
-        const requestPassword = req.body.password;
-        const newlyEncryptedPassword = await encryptData(requestPassword);
-        const returnType = await this.dbService.get({ isUser: true, user: { email: req.body.email, password: newlyEncryptedPassword, name: req.body.name }, rent: null });
-
-        if (returnType === null || returnType === undefined) {
-            res.status(404).json({
-                "message": "user not found"
-            })
-        }
         try {
-            const { user } = returnType!;
-            const oldEncryptedPassword = user!.password;
-
-            if (!await comparePasswords(newlyEncryptedPassword, oldEncryptedPassword)) res.status(404).json({
-                "message": "invalid login credentials"
-            });
-            const accessToken = encryptData(req);
+            const accessToken = generateAccessToken(req);
             res.setHeader("authorization", `Bearer ${accessToken}`);
             res.status(200).json({
                 "message": "login succesful"
-            })
+            }).end();
         } catch (error) {
             res.status(500).json({
                 "message": "server error"
-            })
+            }).end()
         }
     }
 }

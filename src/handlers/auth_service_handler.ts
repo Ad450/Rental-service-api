2f85c3aadbdcfb2d9061edfaae3347040492dbc4
Ad450 <adjeimanue@gmail.com>
2022-07-29 00:23:00 +0000

    depending on trialdb

--- src/handlers/auth_service_handler.ts
+++ src/handlers/auth_service_handler.ts
@@ -1,13 +1,21 @@
 import { NextFunction, Request, Response } from "express";
-import { comparePasswords, encryptData, generateAccessToken, hashData } from "../core/helpers";
+import {
+    comparePasswords,
+    encryptData,
+    generateAccessToken,
+    hashData,
+} from "../core/helpers";
+import { TrialDatabase } from "../db/db1";
 import DatabaseService from "../db/db_service";
 import { DatabaseParam, UserParam } from "../interfaces/database_service_param";
 import ApiResponse from "../response_handlers/response_handler";
+import { User } from "../db/TrialDatabaseImpl";
+import { toNamespacedPath } from "path";
 
 export default class AuthServiceHandler {
-    dbService: DatabaseService<DatabaseParam>;
+    dbService: TrialDatabase;
 
-    constructor(dbService: DatabaseService<DatabaseParam>) {
+    constructor(dbService: TrialDatabase) {
         this.dbService = dbService;
     }
 
@@ -20,27 +28,37 @@ export default class AuthServiceHandler {
             email: req.body.email,
             password: encryptedPassword,
             name: req.body.email,
-        }
+        };
         try {
-            await this.dbService.create({ user: userData, rent: null, isUser: true });
+            await this.dbService.create<User>({
+                name: req.body.email,
+                email: encryptedPassword,
+                password: req.body.email,
+            });
 
             const accessToken = await generateAccessToken(req);
             res.setHeader("authorization", `Bearer ${accessToken}`);
 
-            res.status(200).set("content-type", "application/json").json(userData).end();
+            res
+                .status(200)
+                .set("content-type", "application/json")
+                .json(userData)
+                .end();
         } catch (error) {
-            res.status(404).json(ApiResponse.responseJson(ApiResponse.responses.userAlreadyExists)).end();
-        }
-
-    }
-
-    async login(req: Request, res: Response, next: NextFunction): Promise<void> {
-        try {
-            const accessToken = generateAccessToken(req);
-            res.setHeader("authorization", `Bearer ${accessToken}`);
-            res.status(200).json(ApiResponse.responseJson(ApiResponse.responses.loginSuccessful)).end();
-        } catch (error) {
-            res.status(500).json(ApiResponse.responseJson(ApiResponse.responses.serverError)).end()
+            res
+                .status(404)
+                .json(ApiResponse.responseJson(ApiResponse.responses.userAlreadyExists))
+                .end();
         }
     }
+
+    // async login(req: Request, res: Response, next: NextFunction): Promise<void> {
+    //     try {
+    //         const accessToken = generateAccessToken(req);
+    //         res.setHeader("authorization", `Bearer ${accessToken}`);
+    //         res.status(200).json(ApiResponse.responseJson(ApiResponse.responses.loginSuccessful)).end();
+    //     } catch (error) {
+    //         res.status(500).json(ApiResponse.responseJson(ApiResponse.responses.serverError)).end()
+    //     }
+    // }
 }

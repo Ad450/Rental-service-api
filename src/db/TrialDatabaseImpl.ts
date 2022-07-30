22105dd6a519f4de25bf1fa3670f1d9340f52c3d
Ad450 <adjeimanue@gmail.com>
2022-07-30 14:38:27 +0000

    type guard implementation in trial db

--- src/db/TrialDatabaseImpl.ts
+++ src/db/TrialDatabaseImpl.ts
@@ -1,51 +1,69 @@
-import { TrialDatabase } from "./db1";
+import { TrialDatabase } from "./TrialDatabase";
 import { BookModel, UserModel } from "./db_setup";
+import { ITypeGuard, typeGuard } from "../core/helpers";
 
-const UserModelType = typeof UserModel;
+//const UserModelType = typeof UserModel;
 
 export class User {
-    email: string | null | undefined;
-    password: string | null | undefined;
-    name: string | null | undefined;
+  email: string | null | undefined;
+  password: string | null | undefined;
+  name: string | null | undefined;
 }
 export class Book {
-    name: string | null | undefined;
-    hash: string | null | undefined;
-    isRented: boolean | null | undefined;
-    startDate: string | null | undefined;
-    endDate: string | null | undefined;
-    rentedBy: string | null | undefined;
+  name: string | null | undefined;
+  hash: string | null | undefined;
+  isRented: boolean | null | undefined;
+  startDate: string | null | undefined;
+  endDate: string | null | undefined;
+  rentedBy: string | null | undefined;
 }
 
 export default class TrialDatabaseImpl implements TrialDatabase {
-    async create<T extends User | Book>(param: T): Promise<void> {
-        if (param instanceof User) {
-            const { email, password, name } = param;
-            const existingUser = await UserModel.findOne({ email: email });
+  async create<T extends User | Book>(param: T): Promise<void> {
+    const types: ITypeGuard = {
+      param: typeof param,
+      typeArray: [User, Book],
+    };
+    if (!typeGuard(types)) {
+      // testing
+      console.log("got here");
+      const { email, password, name } = param as User;
+      // testing.........
+      console.log(email);
+      console.log(name);
+      console.log(password);
 
-            if (existingUser) throw new Error("user already exists");
-            const user = new UserModel({
-                email: email,
-                password: password,
-                name: name,
-            });
-            await user.save();
-            return;
-        } else {
-            const { name, hash, isRented, startDate, endDate, rentedBy } = param;
-            const existingBook = await BookModel.findOne({ name: name });
-            if (existingBook) throw new Error("Book already exists");
+      const existingUser = await UserModel.findOne({ email: email });
 
-            const book = new BookModel({
-                name: name,
-                hash: hash,
-                isRented: isRented,
-                startDate: startDate,
-                endDate: endDate,
-                rentedBy: rentedBy,
-            });
-            await book.save();
-            return;
-        }
+      if (existingUser) throw new Error("user already exists");
+      else {
+        const user = new UserModel({
+          email: email,
+          password: password,
+          name: name,
+        });
+        await user.save();
+        return;
+      }
+    } else {
+      // testing
+      console.log("got intno books");
+
+      const { name, hash, isRented, startDate, endDate, rentedBy } =
+        param as Book;
+      const existingBook = await BookModel.findOne({ name: name });
+      if (existingBook) throw new Error("Book already exists");
+
+      const book = new BookModel({
+        name: name,
+        hash: hash,
+        isRented: isRented,
+        startDate: startDate,
+        endDate: endDate,
+        rentedBy: rentedBy,
+      });
+      await book.save();
+      return;
     }
+  }
 }

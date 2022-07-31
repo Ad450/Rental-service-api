b22d34cd26b7f736429a8293f2230a326ce2249f
Ad450 <adjeimanue@gmail.com>
2022-07-31 01:09:39 +0000

    minor changes and formatting

--- src/index.ts
+++ src/index.ts
@@ -20,26 +20,59 @@ const turnInBookIndex = 5;
 const getBookIndex = 6;
 const getAllBooksIndex = 7;
 
-
-app.post(routers[signupIndex].route, routers[signupIndex].middlewares, routers[signupIndex].handlers);
-app.post(routers[loginIndex].route, routers[loginIndex].middlewares, routers[loginIndex].handlers);
-app.post(routers[getUserIndex].route, routers[getUserIndex].middlewares, routers[getUserIndex].handlers);
-app.post(routers[getAllUsersIndex].route, routers[getAllUsersIndex].middlewares, routers[getAllUsersIndex].handlers);
-app.post(routers[rentBookIndex].route, routers[rentBookIndex].middlewares, routers[rentBookIndex].handlers);
-app.post(routers[turnInBookIndex].route, routers[turnInBookIndex].middlewares, routers[turnInBookIndex].handlers);
-app.post(routers[getBookIndex].route, routers[getBookIndex].middlewares, routers[getBookIndex].handlers);
-app.post(routers[getAllBooksIndex].route, routers[getAllBooksIndex].middlewares, routers[getAllBooksIndex].handlers);
+app.post(
+  routers[signupIndex].route,
+  routers[signupIndex].middlewares,
+  routers[signupIndex].handlers
+);
+app.post(
+  routers[loginIndex].route,
+  routers[loginIndex].middlewares,
+  routers[loginIndex].handlers
+);
+app.post(
+  routers[getUserIndex].route,
+  routers[getUserIndex].middlewares,
+  routers[getUserIndex].handlers
+);
+app.post(
+  routers[getAllUsersIndex].route,
+  routers[getAllUsersIndex].middlewares,
+  routers[getAllUsersIndex].handlers
+);
+app.post(
+  routers[rentBookIndex].route,
+  routers[rentBookIndex].middlewares,
+  routers[rentBookIndex].handlers
+);
+app.post(
+  routers[turnInBookIndex].route,
+  routers[turnInBookIndex].middlewares,
+  routers[turnInBookIndex].handlers
+);
+app.post(
+  routers[getBookIndex].route,
+  routers[getBookIndex].middlewares,
+  routers[getBookIndex].handlers
+);
+app.post(
+  routers[getAllBooksIndex].route,
+  routers[getAllBooksIndex].middlewares,
+  routers[getAllBooksIndex].handlers
+);
 
 const startApp = async () => {
-    try {
-        app.listen(process.env.SERVER_PORT || 3000, () => {
-            console.log("sever started on port " + process.env.SERVER_PORT + ", more fire");
-        });
-        await connectMongoose()
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    app.listen(process.env.SERVER_PORT || 3000, () => {
+      console.log(
+        "sever started on port " + process.env.SERVER_PORT + ", more fire"
+      );
+    });
+    await connectMongoose();
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-/// execution starts here 
+/// Program execution starts here
 startApp();

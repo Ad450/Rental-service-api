1639015ee7fb45022f26cb41a80fa379e5ff6c67
Ad450 <adjeimanue@gmail.com>
2022-08-28 15:37:42 +0000

    moved to mongoDB directory

---
+++ src/db/mongoDB/TrialDatabase.ts
@@ -0,0 +1,4 @@
+import { Book, User } from "./TrialDatabaseImpl";
+export abstract class TrialDatabase {
+  abstract create<T extends Book | User>(param: T): Promise<void>;
+}
---
+++ src/db/mongoDB/db_setup.ts
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+const bookSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  hash: {
+    type: String,
+    required: true,
+  },
+  startDate: {
+    type: String, // will be update to Date type
+    required: false,
+  },
+  endDate: {
+    type: String, // will be update to Date type
+    required: false,
+  },
+  isRented: {
+    type: Boolean,
+    required: true,
+  },
+  rentedBy: {
+    type: String,
+    required: true,
+  },
+});
+
+export const connectMongoose = async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE_URL || "");
+    console.log("mongoose connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const BookModel = mongoose.model("Book", bookSchema);
+export const UserModel = mongoose.model("User", userSchema);
